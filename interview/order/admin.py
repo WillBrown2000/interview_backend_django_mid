372c8a758ea4a3a313e7996acae44bf542ac96d0
Will Brown <will.j.brown.2@gmail.com>
2024-06-30 17:50:55 -0600

    first pass on django models and admin section

--- interview/order/admin.py
+++ interview/order/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order, OrderTag
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','created_at')
+    ordering = ('-created_at',)
+
+@admin.register(OrderTag)
+class OrderTagAdmin(admin.ModelAdmin):
+    list_display = ('id',)
